b1a08876d2e31a28a3f041ac51c22917f238440c
holgerd77 <Holger.Drewes@googlemail.com>
2014-10-22 13:19:11 +0200

    Working on IN_DGH_EXP (India DGH Exploration) scraper

---
+++ scrapy_ooc/scrapy_ooc/spiders/IN_DGH_EXP.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+import json
+import scrapy
+from scrapy.contrib.linkextractors import LinkExtractor
+from scrapy_ooc.item_loaders import ConcessionLoader
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.http import Request
+
+from scrapy_ooc.items import InDghExpItem
+
+
+class InDghExpSpider(CrawlSpider):
+    name = 'IN_DGH_EXP'
+    allowed_domains = ['dghindia.org']
+    start_urls = ['http://dghindia.org/PSC_D.aspx?tab=0']
+    
+    def parse_work_program_table(self, table_sel):
+        table_list = [[u'Phase', u'2D(LKM)', u'3D(SKM)', u'Wells',],]
+        tr_sel_list = table_sel.xpath('.//tr[not(@class="TableHead") and not(@align="right")]')
+        for tr_sel in tr_sel_list:
+            row_list = []
+            row_list.append(tr_sel.xpath('(./td)[1]/text()').extract()[0])
+            row_list.append(tr_sel.xpath('(./td)[2]/text()').extract()[0])
+            row_list.append(tr_sel.xpath('(./td)[3]/text()').extract()[0])
+            row_list.append(tr_sel.xpath('(./td)[4]/text()').extract()[0])
+            table_list.append(row_list)
+            
+        return json.dumps(table_list)
+    
+    
+    def parse_detail_page(self, response):
+        l = response.meta['l']
+        xpath_base = '//table[@id="ctl00_ContentDGH_detailsVwBlockStatus"]//'
+        
+        status_fields = [
+            ('block_name', 'Block Name'),
+            ('round', 'Round'),
+            ('operator', 'Operator'),
+            ('consortium', 'Consortium'),
+            ('status', 'Status'),
+            ('basin', 'Basin'),
+            ('state', 'State'),
+            ('date_of_signing', 'Date of Signing'),
+            ('effective_date', 'Effective Date'),
+            ('initial_area', 'Initial Area'),
+            ('present_area', 'Present Area'),
+            ('relinquished_area', 'Relinquised Area'),
+        ]
+        
+        for sf in status_fields:
+            xpath = xpath_base + 'tr[contains(td, "' + sf[1] + '")]/td[2]/text()'
+            l.add_value(sf[0], response.xpath(xpath).extract())
+        
+        minimum_work_program_table = u''
+        xpath = '//table[@id="ctl00_ContentDGH_grdVwBlockMWP"]'
+        sel_list = response.xpath(xpath)
+        if len(sel_list) > 0:
+            minimum_work_program_table = self.parse_work_program_table(sel_list[0])
+        l.add_value('minimum_work_program_table', minimum_work_program_table)
+        
+        yield l.load_item()
+    
+    
+    def parse(self, response):
+        urls = response.xpath('//table[@id="ctl00_ContentDGH_grdVwAllBlocks"]//tr/td/a/@href')
+            
+        # Complete: "...urls:", Extract: "...urls[0:3]:"
+        for url in urls[0:3]:
+            url = 'http://dghindia.org/' + url.re('"", "([^"]*)')[0]
+            l = ConcessionLoader(item=InDghExpItem(), response=response)
+            l.add_value('url', url)
+            yield Request(url, callback=self.parse_detail_page, meta={'l': l})
+            #l.add_value('partenaire', table.xpath('.//table[@class="tab_concess"]//tr[not(@class="title")]/td[1]/p/text()').extract())
+            #l.add_value('participation', table.xpath('.//table[@class="tab_concess"]//tr[not(@class="title")]/td[2]/p/text()').extract())
+            #yield Request(url, callback=self.parse_detail_page, meta={'l': l})
